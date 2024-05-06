bb0926c8960fd99c5fbd31567893d7d8a4ef2500
H-Thien11 <vlht2003@gmail.com>
2024-05-06 14:33:24 +0700

    Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,13 +2,19 @@ import {View, Text, ImageBackground} from 'react-native';
 import React from 'react';
 import styles from '../Home/style';
 
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 export default function index() {
   return (
     <ImageBackground
       resizeMode="stretch"
       source={require('../../assets/img/background.png')}
       style={styles.imageBG}>
-        
-      </ImageBackground>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </ImageBackground>
   );
 }
