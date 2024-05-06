d853dafff5f3718ab6d53b7e70be953e2b2974d1
H-Thien11 <vlht2003@gmail.com>
2024-05-06 13:59:40 +0700

    home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,14 @@
-import {View, Text} from 'react-native';
+import {View, Text, ImageBackground} from 'react-native';
 import React from 'react';
+import styles from '../Home/style';
 
 export default function index() {
-  return <View></View>;
+  return (
+    <ImageBackground
+      resizeMode="stretch"
+      source={require('../../assets/img/background.png')}
+      style={styles.imageBG}>
+        
+      </ImageBackground>
+  );
 }
--- src/screens/Home/style.tsx
+++ src/screens/Home/style.tsx
@@ -1,6 +1,14 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  // background
+  imageBG: {
+    flex: 1,
+    width: '100%',
+    height: '108%',
+    bottom: '8%',
+  },
+});
 
 export default styles
 
