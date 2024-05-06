8a9aaefd8b25ddd11ca4b2108dc9f7cdb6ff6e1a
huyhoangex <huyhoangex.dev@gmail.com>
2024-05-06 15:48:24 +0700

    hi

--- src/screens/AuthForm/Resend/KYC/index.tsx
+++ src/screens/AuthForm/Resend/KYC/index.tsx
@@ -15,14 +15,18 @@ import {
   import Clipboard from '@react-native-clipboard/clipboard';
   import InputOTP from '../../../../components/InputOTP';
   import { useNavigation } from '@react-navigation/native';
-<<<<<<< HEAD
-=======
-  
->>>>>>> parent of a15ef6c (Merge branch 'main' of https://github.com/H-Thien11/GiaoDienEgfarm)
-  
+  import DropDownPicker from 'react-native-dropdown-picker';  
   
   export default function Index() {
-     const navigation = useNavigation() as any;
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [items, setItems] = useState([
+      {label: 'CMND or CCCD', value: 'cmndorcccd'},
+      {label: 'Driving permit', value: 'drivingpermit'},
+      {label: 'Passport', value: 'passport'},
+      {label: 'Residence permit', value: 'residencepermit'},
+    ]);
+    const navigation = useNavigation() as any;
     const input = useRef<OTPTextView>(null);
     const handleCellTextChange = async (text: any, i: any) => {
       if (i === 0) {
@@ -38,51 +42,62 @@ import {
         source={require('../../../../assets/img/background.png')}
         style={styles.imageBG}>
         <View style={{flex: 1, top: '8%'}}>
-          {/* thanh tiêu đề */}
+          {/* title */}
           <View style={styles.title}>
             <TouchableOpacity
               style={styles.goback}
               activeOpacity={0.3}
-              onPress={() => navigation.goback()}>
+              onPress={() => navigation.goBack()}>
               <Image
                 style={{width: 8, height: 13}}
                 source={require('../../../../assets/img/back.png')}></Image>
             </TouchableOpacity>
-            <Text style={[styles.texttitle, styles.font]}>
-              KYC
-            </Text>
+            <Text style={[styles.texttitle, styles.font]}>KYC</Text>
           </View>
+          {/* secsion */}
           <View>
+            {/* select dropdown */}
             <View>
-<<<<<<< HEAD
+              {/* text */}
               <Text style={[styles.textselect, styles.font]}>
                 Select your ID type
               </Text>
-              <View style={{flex: 1}}>
-                
-              </View>
-=======
-              <Text style={[styles.textselect, styles.font]}>Select your ID type</Text>
->>>>>>> parent of a15ef6c (Merge branch 'main' of https://github.com/H-Thien11/GiaoDienEgfarm)
+              {/* select */}
+              <LinearGradient
+                locations={[0, 1]}
+                colors={['rgba(12, 244, 250, 0.2)', 'rgba(25, 151, 153, 0.2)']}
+                start={{x: 0, y: 0}}
+                end={{x: 0, y: 1}}
+                style={styles.gradientInput}>
+                <DropDownPicker
+                  open={open}
+                  value={value}
+                  items={items}
+                  setOpen={setOpen}
+                  setValue={setValue}
+                  setItems={setItems}
+                  theme="DEFAULT"
+                  placeholder="Select your ID type"
+                />
+              </LinearGradient>
             </View>
-          
+
             {/* button */}
             <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.3}
-                onPress={() => navigation.navigate('Layout2')}>
-                <LinearGradient
+              style={styles.button}
+              activeOpacity={0.3}
+              onPress={() => navigation.navigate('Layout2')}>
+              <LinearGradient
                 locations={[0, 1]}
                 colors={['#2AFCFF', '#00FB91']}
                 start={{x: 1, y: 0}}
                 end={{x: 0, y: 1}}
                 style={styles.gradient}>
                 <Text style={[styles.resend, styles.font]}>RESEND</Text>
-                </LinearGradient>
+              </LinearGradient>
             </TouchableOpacity>
           </View>
-        </View>            
-
+        </View>
       </ImageBackground>
     );
   }
